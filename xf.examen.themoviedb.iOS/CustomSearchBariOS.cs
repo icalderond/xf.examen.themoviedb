bbc6e6dc77c4ce4b9920661732f56287549a2a96
Israel Calderon de la Cruz <icalderond@outlook.com>
2021-10-29 13:19:36 -0500

    Adding customrenderer to fix bug

---
+++ xf.examen.themoviedb.iOS/CustomSearchBariOS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using xf.examen.themoviedb.CustomRender;
+using xf.examen.themoviedb.iOS;
+
+[assembly: ExportRenderer(typeof(CustomSearchBar), typeof(CustomSearchBariOS))]
+namespace xf.examen.themoviedb.iOS
+{
+    public class CustomSearchBariOS : SearchBarRenderer
+    {
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == SearchBar.TextProperty.PropertyName)
+                Control.Text = Element.Text;
+
+            if (e.PropertyName != SearchBar.CancelButtonColorProperty.PropertyName && e.PropertyName != SearchBar.TextProperty.PropertyName)
+                base.OnElementPropertyChanged(sender, e);
+
+        }
+    }
+}
+
---
+++ xf.examen.themoviedb/CustomRender/CustomSearchBar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace xf.examen.themoviedb.CustomRender
+{
+    public class CustomSearchBar : SearchBar
+    {
+        public CustomSearchBar()
+        {
+        }
+    }
+}
