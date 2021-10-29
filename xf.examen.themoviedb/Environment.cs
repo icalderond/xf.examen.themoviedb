d55d34f6e3e98692571dd420cb20ad0605f92909
Israel Calderon de la Cruz <icalderond@outlook.com>
2021-10-29 13:39:11 -0500

    Se agrega clave para consulta

---
+++ xf.examen.themoviedb/Environment.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace xf.examen.themoviedb
+{
+    public static class Environment
+    {
+        // Get apikey from this site: https://developers.themoviedb.org/
+        public static string THEMOVIEDB_API_KEY = "14125976e5d49d14d7093badd73f1ff6";
+    }
+}
